3d851a89340b17f43cc6002154b22fa54c648ace
Christian <sweatchristian03@gmail.com>
2022-10-04 16:16:29 -0400

    Created ProjectController.java an implementation of Controller.java

---
+++ src/main/java/me/christian/controller/implementation/ProjectController.java
@@ -0,0 +1,27 @@
+package me.christian.controller.implementation;
+
+import me.christian.controller.Controller;
+import spark.Route;
+
+public class ProjectController implements Controller {
+
+    @Override
+    public void get(String path, Route route) {
+
+    }
+
+    @Override
+    public void post(String path, Route route) {
+
+    }
+
+    @Override
+    public void put(String path, Route route) {
+
+    }
+
+    @Override
+    public void delete(String path, Route route) {
+
+    }
+}
