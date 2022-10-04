eba0ce3d94127709849918a029a7a39250101c61
Christian <sweatchristian03@gmail.com>
2022-10-04 16:18:02 -0400

    Added ControllerFactory reference and initialization call.

--- src/main/java/me/christian/App.java
+++ src/main/java/me/christian/App.java
@@ -1,13 +1,21 @@
 package me.christian;
 
+import me.christian.factory.implementation.ControllerFactory;
+
 import static spark.Spark.*;
+
 /**
  * @author Christian Sweat
  */
 public class App 
 {
+    private final static ControllerFactory CONTROLLER_FACTORY = new ControllerFactory();
+
     public static void main( String[] args )
     {
-        get("/hello", (req, res) -> "Hello World");
+        // Landing Page
+        get("/", (req, res) -> "Hello World! Try navigating to /(project name)/(class name)");
+
+        CONTROLLER_FACTORY.initialize();
     }
 }
