ca902efd2b33ea344207e65642b1ec968da94b8d
Christian <sweatchristian03@gmail.com>
2022-10-09 18:13:45 -0400

    Added GET POST PUT DELETE for ProjectController.java

--- src/main/java/me/christian/controller/implementation/ProjectController.java
+++ src/main/java/me/christian/controller/implementation/ProjectController.java
@@ -1,27 +1,30 @@
 package me.christian.controller.implementation;
 
 import me.christian.controller.Controller;
-import spark.Route;
 
-public class ProjectController implements Controller {
+import static spark.Spark.*;
 
-    @Override
-    public void get(String path, Route route) {
+public class ProjectController extends Controller {
 
+    public ProjectController() {
+        super("Project Controller");
     }
 
     @Override
-    public void post(String path, Route route) {
-
-    }
-
-    @Override
-    public void put(String path, Route route) {
-
-    }
-
-    @Override
-    public void delete(String path, Route route) {
+    protected void establishRoutes() {
+        get("/project/:name", (req, res) -> {
+            String projectName = req.params(":name");
+            return "get database information for " + projectName;
+        });
+        post("/project/:name", (req, res) -> {
+            return "add new record to database";
+        });
+        put("/project/:name", (req, res) -> {
+            return "modify record in database";
+        });
+        delete("/project/:name", (req, res) -> {
+            return "delete record in database";
+        });
 
     }
 }
