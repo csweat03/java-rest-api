49fb1b8555c94d170e651111c802707fd606fb00
Christian <sweatchristian03@gmail.com>
2022-10-16 16:02:02 -0400

    Modify UserController.java, Ported User Logic over to UserApplication.java

---
+++ src/main/java/me/christian/controller/implementation/UserController.java
@@ -0,0 +1,34 @@
+package me.christian.controller.implementation;
+
+import com.mongodb.client.MongoCollection;
+import me.christian.App;
+import me.christian.application.implementation.UserApplication;
+import me.christian.controller.Controller;
+import org.bson.Document;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.*;
+
+public class UserController extends Controller {
+
+    public UserController() {
+        super("User Controller");
+    }
+
+    @Override
+    protected void establishRoutes() {
+        MongoCollection<Document> usersCollection = App.getUserDatabase().getCollection("users");
+
+
+
+        LinkedHashMap<String, Object> administratorData = new LinkedHashMap<>();
+
+        administratorData.put("username", "csweat03");
+        administratorData.put("password", "password");
+        administratorData.put("role-type", "administrator");
+//        administratorData.put("api-key", new User("your", "mom").generateApiKey());
+
+        usersCollection.insertOne(new Document(administratorData));
+    }
+}
